9c3322486292d34bc0939d89920b8de173d73b07
nayastronaut <naylabrizola@outlook.com>
2024-07-03 18:12:15 -0300

    aaaa

---
+++ O.nuvelo2/mudar.js
@@ -0,0 +1,31 @@
+const { listarProjetos } = require ('./listar')
+
+
+function mudarProjeto(rl, croche, callback ){
+    listarProjetos(croche)
+        rl.question('Digite o número do projeto que deseja mudar: ', (numero) => {
+            if(numero > 0 && numero <= croche.length){
+                rl.question('Digite o novo nome do projeto: ', (nome) => {
+                    rl.question('Digite o novo fio/material usado: ', (fio) => {
+                        rl.question('Nova data de entrega: ', (data) => {
+                            rl.question('Novo preço: ', (preco) => {
+                                croche[numero - 1] = {
+                                    nome: nome, 
+                                    fio: fio,
+                                    data: data, 
+                                    preco: parseFloat(preco),
+                                }
+                                console.log('Projeto alterado com sucesso!!!')
+                                callback()
+                            })                           
+                        })                       
+                    })
+                })
+            } else {
+                console.log('Número inválido, tente novamente!')
+                callback()
+            }
+        })
+}
+
+module.exports = { mudarProjeto }
